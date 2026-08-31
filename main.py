713ab8ca0019c708dab5b942480b89c13b4dfbb8
kumarnikhil8675at-beep <kumarnikhil8675at@gmail.com>
2026-08-31 17:28:31 +0530

    contact_mail_setup

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from flask import Flask,render_template,request
 import requests
+import smtplib
 
 response=requests.get("https://api.npoint.io/674f5423f73deab1e9a7")
 response=response.json()
 
 app=Flask(__name__)
-
 
 @app.route("/")
 def home():
@@ -18,13 +18,30 @@ def nextt():
     body = request.args.get("body")
     return render_template("post.html",title=title,subtitle=subtitle,body=body)
 
-@app.route("/contact")
-def contact():
-    return render_template("contact.html")
-
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+@app.route("/contact",methods=["POST","Get"])
+def contact():
+    if request.method == "POST":
+        print("function start")
+        Name=request.form['name']
+        Email=request.form['email']
+        Phone=request.form['phone']
+        Messages=request.form['message']
+        sendmail(Name,Email,Phone,Messages)
+        return render_template("contact.html",msg=True)
+    return render_template("contact.html",msg=False)
+
+def sendmail(Name,Email,Phone,Messages):
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user="",password="")
+        connection.sendmail(
+            from_addr="",
+            to_addrs="",
+            msg=f"Subject:User Message\n\n Name:{Name}\nEmail:{Email}\nPhone:{Phone}\nMessages:{Messages}")
 
 if __name__ == "__main__":
     app.run(debug=True)
