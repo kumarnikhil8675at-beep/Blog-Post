31f94352a6912e57c9f631912fbdd573ed1b4cab
kumarnikhil8675at-beep <kumarnikhil8675at@gmail.com>
2026-09-15 04:13:25 +0530

    version_RD

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ gravatar = Gravatar(app,
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('sql_key')
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
@@ -246,4 +246,4 @@ def logout():
     return redirect(url_for('get_all_posts'))
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5003)
+    app.run(debug=False)
